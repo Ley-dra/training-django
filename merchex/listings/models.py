b6eb4d6c78f3096d9a38f6706182d4cbd8d99957
Morgane FLORES <morgane.flores@neuf.fr>
2023-05-15 09:04:58 +0200

    end part 2 : better model

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,7 +1,38 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
+    def __str__(self):
+        return f'{self.name}'
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+        METAL = "M"
+        
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+    validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
     
 class Listing(models.Model):
+    def __str__(self):
+        return f'{self.title}'
+    
+    class Type(models.TextChoices):
+        RECORDS = 'R'
+        CLOTHING = 'C'
+        POSTER = 'P'
+        MISCELLANEOUS = 'M'
     title = models.fields.CharField(max_length=200)
+    decription = models.fields.CharField(max_length=500)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2023)])
+    type = models.fields.CharField(choices=Type.choices, max_length=5)
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
