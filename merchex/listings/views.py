bde5e49f2ab5403a75ffdb60d995b4506fc0391a
Morgane FLORES <morgane.flores@neuf.fr>
2023-05-15 10:28:12 +0200

    Adding link between pages

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -2,21 +2,33 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from listings.models import Band, Listing
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
     return render(request,
-            'listings/hello.html',
+            'listings/band_list.html',
             {'bands': bands})
+    
+def band_detail(request, band_id):
+    band = Band.objects.get(id=band_id)
+    return render(request,
+          'listings/band_detail.html',
+         {'band': band})
 
 def about(request):
     return render(request,
             'listings/about.html')
 
-def listings(request):
+def listing_list(request):
     lists = Listing.objects.all()
     return render(request,
-            'listings/listings.html',
+            'listings/listing_list.html',
             {'lists': lists})
+    
+def listing_detail(request, list_id):
+    list = Listing.objects.get(id=list_id)
+    return render(request,
+          'listings/listing_detail.html',
+         {'list': list})
 
 def contact(request):
     return render(request,
